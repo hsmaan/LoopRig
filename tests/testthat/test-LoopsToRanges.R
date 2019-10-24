5208cb0bde8665cc62c3e2742ecde63af5dc5967
H Maan <hassaanmaan@hotmail.com>
2019-10-23 23:55:22 -0400

    eod2

---
+++ tests/testthat/test-LoopsToRanges.R
@@ -0,0 +1,13 @@
+library(LoopRig)
+
+# Load files
+
+ovary_loops <- system.file("extdata/loops", "ovary_hg19.bedpe", package = "LoopRig", mustWork = TRUE)
+pancreas_loops <- system.file("extdata/loops", "pancreas_hg19.bedpe", package = "LoopRig", mustWork = TRUE)
+spleen_loops <- system.file("extdata/loops", "spleen_hg19.bedpe", package = "LoopRig", mustWork = TRUE)
+
+test_that("error handling", {
+  
+  expect_error(LoopsToRanges(ovary_loops, ovary_loops), "*Duplicate BEDPE data-files entered*")
+  
+})
